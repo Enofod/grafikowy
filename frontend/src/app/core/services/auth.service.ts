259d3d544c14f9372971f9da87157face7d7c1c8
Dawid Kunert <enofod@gmail.com>
2017-11-11 17:15:29 +0100

    Add auth guards

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ApiClientService } from './api-client.service';
 import { Observable } from 'rxjs/Observable';
+import { JwtHelper} from 'angular2-jwt';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
+
 @Injectable()
 export class AuthService {
+
+  jwtHelper: JwtHelper = new JwtHelper();
+  tokenFieldName = 'authToken';
 
   constructor(private apiClientService: ApiClientService) { }
 
@@ -16,10 +22,24 @@ export class AuthService {
 
     return this.apiClientService.postForEmptyResponse('login', new LoginCredentials(email, password)).map(
       (response: Response) => {
-        console.log(response.headers.get('Authorization'));
+        const authToken = response.headers.get('Authorization');
+        localStorage.setItem(this.tokenFieldName, authToken);
         return true;
       }
     );
+  }
+
+  isTokenExpired() {
+    const authToken = localStorage.getItem(this.tokenFieldName);
+    return authToken === null ? true : this.jwtHelper.isTokenExpired(authToken);
+  }
+
+  isAdmin() {
+    const authToken = localStorage.getItem(this.tokenFieldName);
+    if (authToken === null) {
+      return false;
+    }
+    console.log(this.jwtHelper.decodeToken(authToken));
   }
 }
 
