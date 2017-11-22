8d0a9f6c45012a4bea3e3e32916e97399789ac52
Dawid Kunert <enofod@gmail.com>
2017-11-22 22:25:24 +0100

    server backend url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,9 +3,11 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+//  apiUrl: 'http://80.211.194.224:8090/',
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8090/',
+  apiUrl: 'http://80.211.194.224:8090/',
   authTokenLocalStorageKey: 'grafikowyAuthToken',
   themeColor: 'grafikowyThemeColor'
 };
