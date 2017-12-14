b1abfe2fa0b5f60b15938939a55f93846002f33b
Dawid Kunert <enofod@gmail.com>
2017-12-14 20:37:14 +0100

    Remove log in user

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -13,7 +13,6 @@ export class UserService {
   constructor(private apiClientService: ApiClientService, private authService: AuthService) { }
 
   getUserByEmail(email: string): Observable<User> {
-    console.log(email);
     return this.apiClientService.get<User>('users/' + email);
   }
 
